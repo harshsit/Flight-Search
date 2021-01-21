df8db2b9de232d16c836ca3855be1898f1e15045
harshsit <harsh.raj@sitpune.edu.in>
2021-01-21 12:34:41 +0530

    Updates

---
+++ precache-manifest.2bb867b86664d73c64ac1325db9be998.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe5dc77a2805d79cf51",
+    "url": "/Flight-Search/static/css/main.78002f5a.chunk.css"
+  },
+  {
+    "revision": "3fe5dc77a2805d79cf51",
+    "url": "/Flight-Search/static/js/main.60f50efb.chunk.js"
+  },
+  {
+    "revision": "84cc1c2f3e615267403d",
+    "url": "/Flight-Search/static/js/runtime~main.b3356391.js"
+  },
+  {
+    "revision": "bcd605d9d9095ef9eea3",
+    "url": "/Flight-Search/static/css/2.3e082295.chunk.css"
+  },
+  {
+    "revision": "bcd605d9d9095ef9eea3",
+    "url": "/Flight-Search/static/js/2.c7342915.chunk.js"
+  },
+  {
+    "revision": "7ac3b97dfe2d14c6a08281326ff11de5",
+    "url": "/Flight-Search/static/media/logo.7ac3b97d.png"
+  },
+  {
+    "revision": "33bf74155d298d9870aa5f54706c8638",
+    "url": "/Flight-Search/static/media/plane-841441_1920.33bf7415.jpg"
+  },
+  {
+    "revision": "6024f09b11a044bf2ecfc5964d7710b3",
+    "url": "/Flight-Search/index.html"
+  }
+];
