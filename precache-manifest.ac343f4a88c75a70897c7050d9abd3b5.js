526bd560557877d43e1675fac08a9119a43038bc
harshsit <harsh.raj@sitpune.edu.in>
2021-01-22 04:33:16 +0530

    Updates

---
+++ precache-manifest.ac343f4a88c75a70897c7050d9abd3b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a0227c8aa1658a348b1",
+    "url": "/static/css/main.c273d9b7.chunk.css"
+  },
+  {
+    "revision": "4a0227c8aa1658a348b1",
+    "url": "/static/js/main.b85422ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bcd605d9d9095ef9eea3",
+    "url": "/static/css/2.3e082295.chunk.css"
+  },
+  {
+    "revision": "bcd605d9d9095ef9eea3",
+    "url": "/static/js/2.c7342915.chunk.js"
+  },
+  {
+    "revision": "7ac3b97dfe2d14c6a08281326ff11de5",
+    "url": "/static/media/logo.7ac3b97d.png"
+  },
+  {
+    "revision": "33bf74155d298d9870aa5f54706c8638",
+    "url": "/static/media/plane-841441_1920.33bf7415.jpg"
+  },
+  {
+    "revision": "b2f22e258c31a1046b742b2fdc192ac1",
+    "url": "/index.html"
+  }
+];
