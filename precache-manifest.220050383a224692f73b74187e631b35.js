73bb720e9aba419d56f29bb4492e3db0bf95d5ca
harshsit <harsh.raj@sitpune.edu.in>
2021-01-22 04:35:20 +0530

    Updates

---
+++ precache-manifest.220050383a224692f73b74187e631b35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff587bfed4ef023c7b28",
+    "url": "/Flight-Search/static/css/main.d5c9298a.chunk.css"
+  },
+  {
+    "revision": "ff587bfed4ef023c7b28",
+    "url": "/Flight-Search/static/js/main.8cac2c57.chunk.js"
+  },
+  {
+    "revision": "84cc1c2f3e615267403d",
+    "url": "/Flight-Search/static/js/runtime~main.b3356391.js"
+  },
+  {
+    "revision": "bcd605d9d9095ef9eea3",
+    "url": "/Flight-Search/static/css/2.3e082295.chunk.css"
+  },
+  {
+    "revision": "bcd605d9d9095ef9eea3",
+    "url": "/Flight-Search/static/js/2.c7342915.chunk.js"
+  },
+  {
+    "revision": "7ac3b97dfe2d14c6a08281326ff11de5",
+    "url": "/Flight-Search/static/media/logo.7ac3b97d.png"
+  },
+  {
+    "revision": "33bf74155d298d9870aa5f54706c8638",
+    "url": "/Flight-Search/static/media/plane-841441_1920.33bf7415.jpg"
+  },
+  {
+    "revision": "437aa17c73638b22ad388a6c193e6d60",
+    "url": "/Flight-Search/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac343f4a88c75a70897c7050d9abd3b5.js"
+  "/Flight-Search/precache-manifest.220050383a224692f73b74187e631b35.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Flight-Search/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
